685d6a920d71187e68742d0f47e09df45c8643e2
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2022-01-06 12:38:15 -0300

    Adiciona configuração de segurança para criar usuário inicial em banco

--- src/main/java/br/com/abruzzo/tqi_backend_evolution_2021/config/WebSecurityConfig.java
+++ src/main/java/br/com/abruzzo/tqi_backend_evolution_2021/config/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package br.com.abruzzo.tqi_backend_evolution_2021.config;
 
+import br.com.abruzzo.tqi_backend_evolution_2021.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @author Emmanuel Abruzzo
@@ -12,6 +20,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private DataSource dataSource;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,6 +45,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     logout.logoutUrl("/logout")
                             .logoutSuccessUrl("/home");
                 }).csrf().disable();
+    }
+
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder);
+
+        UserDetails usuarioInicial = User.withDefaultPasswordEncoder()
+                .username("99999999999")
+                .password("9999")
+                .roles(String.valueOf(Role.FUNCIONARIO))
+                .roles(String.valueOf(Role.SUPER_ADMIN))
+                .build();
+
     }
 
 
