c3389f6991fec571158b690b6d68d9594895c49b
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2022-01-06 12:27:01 -0300

    Adiciona configurações de segurança

---
+++ src/main/java/br/com/abruzzo/tqi_backend_evolution_2021/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package br.com.abruzzo.tqi_backend_evolution_2021.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Emmanuel Abruzzo
+ * @date 04/01/2022
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/home/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl(String.format("/home"), true)
+                        .permitAll()
+                )
+                .logout(logout -> {
+                    logout.logoutUrl("/logout")
+                            .logoutSuccessUrl("/home");
+                }).csrf().disable();
+    }
+
+
+
+}
+
